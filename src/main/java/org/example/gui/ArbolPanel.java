d3f3de434cb94d55193dd97dfa20497b438847f9
Luis Enrique <104203971+LuisEnriqueGM03@users.noreply.github.com>
2023-06-11 18:31:56 -0400

    a

--- src/main/java/org/example/gui/ArbolPanel.java
+++ src/main/java/org/example/gui/ArbolPanel.java
@@ -4,24 +4,51 @@ import org.example.objetos.Arbol;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class ArbolPanel extends JPanel {
+public class ArbolPanel extends JPanel implements MouseListener {
     private Arbol<String> modelo;
+    private DibujoArbol dibujo;
 
     public ArbolPanel(Arbol<String> modelo) {
         this.modelo = modelo;
-    }
+        this.dibujo = new DibujoArbol(modelo);
 
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(600,500);
+        // Registra el ArbolPanel como receptor de eventos de clic
+        this.addMouseListener(this);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        DibujoArbol dibujo = new DibujoArbol(modelo);
         dibujo.dibujar(g);
     }
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(600,500);
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int xClic = e.getX();
+        int yClic = e.getY();
+
+        String nombreNodo = dibujo.obtenerNodoClic(xClic, yClic);
+        if (nombreNodo != null) {
+            JOptionPane.showMessageDialog(this, "Persona: " + nombreNodo);
+        }
+    }
+
+    // Implementa los demás métodos de MouseListener, aunque no los utilicemos
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
 }
--- src/main/java/org/example/gui/DibujoArbol.java
+++ src/main/java/org/example/gui/DibujoArbol.java
@@ -3,49 +3,55 @@ package org.example.gui;
 import org.example.objetos.Arbol;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.*;
 
 public class DibujoArbol {
     private static final int ESPACIO_HORIZONTAL = 20;
     private static final int ESPACIO_VERTICAL = 50;
     private static final int TAMANO_NODO = 50;
     private final Arbol<String> modelo;
+    private HashMap<Rectangle, String> nodoCoordenadas;
 
     public DibujoArbol(Arbol<String> modelo) {
         this.modelo = modelo;
+        this.nodoCoordenadas = new HashMap<>();
     }
 
     public void dibujar(Graphics g) {
         dibujarNodo(modelo.getRaiz(), 100, 100, g);
     }
 
-    public void dibujarNodo(Arbol.Nodo<String> nodo,
-                            int x, int y, Graphics g) {
+    public void dibujarNodo(Arbol.Nodo<String> nodo, int x, int y, Graphics g) {
         int ancho = ancho(nodo);
 
-        int xNodo = x +ancho/2 - (TAMANO_NODO/2);
+        int xNodo = x + ancho / 2 - (TAMANO_NODO / 2);
 
         int xHijo = x;
         int yHijo = y + TAMANO_NODO + ESPACIO_VERTICAL;
-        for(Arbol.Nodo<String> hijo : nodo.getHijos()) {
-
+        for (Arbol.Nodo<String> hijo : nodo.getHijos()) {
             int avanzar = ancho(hijo) + ESPACIO_HORIZONTAL;
 
             g.setColor(Color.black);
-            g.drawLine(xNodo + TAMANO_NODO/2,
-                    y + TAMANO_NODO / 2,
-                    xHijo + (avanzar-ESPACIO_HORIZONTAL)/2,
-                    yHijo + TAMANO_NODO/2);
+            g.drawLine(xNodo + TAMANO_NODO / 2, y + TAMANO_NODO / 2, xHijo + (avanzar - ESPACIO_HORIZONTAL) / 2,
+                    yHijo + TAMANO_NODO / 2);
 
             dibujarNodo(hijo, xHijo, yHijo, g);
             xHijo += avanzar;
         }
+
+        Rectangle nodoRect = new Rectangle(xNodo, y, TAMANO_NODO, TAMANO_NODO);
+        nodoCoordenadas.put(nodoRect, nodo.getContenido());
+
         g.setColor(Color.white);
-        g.fillOval(xNodo, y,
-                TAMANO_NODO, TAMANO_NODO);
+        g.fillOval(xNodo, y, TAMANO_NODO, TAMANO_NODO);
         g.setColor(Color.black);
-        g.drawOval(xNodo, y,
-                TAMANO_NODO, TAMANO_NODO);
-        g.drawString(nodo.getContenido(),xNodo + 10, y+20);
+        g.drawOval(xNodo, y, TAMANO_NODO, TAMANO_NODO);
+        g.drawString(nodo.getContenido(), xNodo + 10, y + 20);
     }
 
     public int ancho(Arbol.Nodo<String> nodo) {
@@ -55,11 +61,21 @@ public class DibujoArbol {
 
         int ancho = 0;
         int espacio = 0;
-        for(Arbol.Nodo<String> hijo : nodo.getHijos()) {
+        for (Arbol.Nodo<String> hijo : nodo.getHijos()) {
             int avanzar = espacio + ancho(hijo);
             ancho += avanzar;
             espacio = ESPACIO_HORIZONTAL;
         }
         return ancho;
+    }
+
+    public String obtenerNodoClic(int xClic, int yClic) {
+        for (Map.Entry<Rectangle, String> entry : nodoCoordenadas.entrySet()) {
+            Rectangle rect = entry.getKey();
+            if (rect.contains(xClic, yClic)) {
+                return entry.getValue();
+            }
+        }
+        return null;
     }
 }
